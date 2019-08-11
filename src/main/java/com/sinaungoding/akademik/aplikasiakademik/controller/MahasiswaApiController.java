7137f07765b29e0c9ef7d9ce1bed85e283d5114b
od3ng <noprianto@polinema.ac.id>
2019-08-11 22:56:51 +0700

    1. menambahkan deklarasi logger

--- src/main/java/com/sinaungoding/akademik/aplikasiakademik/controller/MahasiswaApiController.java
+++ src/main/java/com/sinaungoding/akademik/aplikasiakademik/controller/MahasiswaApiController.java
@@ -14,25 +14,20 @@ package com.sinaungoding.akademik.aplikasiakademik.controller;
 
 import com.sinaungoding.akademik.aplikasiakademik.dao.MahasiswaDao;
 import com.sinaungoding.akademik.aplikasiakademik.entity.Mahasiswa;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import java.io.IOException;
 
 @RestController
+@Slf4j
 public class MahasiswaApiController {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(MahasiswaApiController.class.getName());
 
     @Autowired
     private MahasiswaDao mahasiswaDao;
@@ -60,9 +55,8 @@ public class MahasiswaApiController {
         try {
             mahasiswaDao.save(mahasiswa);
             return new ResponseEntity<>("Mahasiswa berhasil ditambahkan", HttpStatus.CREATED);
-        } catch (Exception e) {
-            LOGGER.info("DataAccessException");
-            LOGGER.error(e.getMessage());
+        } catch (Exception e){
+            log.error(e.getMessage());
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -72,7 +66,7 @@ public class MahasiswaApiController {
     public void update(@PathVariable("id") String id, @RequestBody @Valid Mahasiswa mahasiswa) {
         Mahasiswa mhs = mahasiswaDao.findById("id").get();
         if (mhs == null) {
-            LOGGER.warn("Mahasiswa id {} tidak ditemukan", id);
+            log.warn("Mahasiswa id {} tidak ditemukan", id);
             return;
         }
         BeanUtils.copyProperties(mahasiswa, mhs);
