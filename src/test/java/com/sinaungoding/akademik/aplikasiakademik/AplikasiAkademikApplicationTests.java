a95815d2785010c866c15939a77520e2d42cc88c
od3ng <noprianto@polinema.ac.id>
2021-11-07 01:15:21 +0700

    remove javafx

--- src/test/java/com/sinaungoding/akademik/aplikasiakademik/AplikasiAkademikApplicationTests.java
+++ src/test/java/com/sinaungoding/akademik/aplikasiakademik/AplikasiAkademikApplicationTests.java
@@ -4,16 +4,12 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import javafx.application.Application;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.io.IOException;
 
